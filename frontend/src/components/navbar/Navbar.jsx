2cb3d3d94c1d0b96b2adf680c0c682f0d3d25e40
dr0er <ernestxxx123@gmail.com>
2021-07-20 21:24:39 +0200

    Stworzyłem header oraz navbar, zarówno struktury jsx jak i stylizacje css

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import './Navbar.css';
 
-function Navbar() {
-  return <section>Navbar</section>;
+function Navbar () {
+
+  return (
+      <section className="navbar">
+        <a href="/" className="navbar-item">Strona główna</a>
+        <a href="/about" className="navbar-item">Kategorie</a>
+        <a href="/portfolio" className="navbar-item">O nas</a>
+        <a href="/shop" className="navbar-item">Kontakt</a>
+      </section>
+  )
+
 }
 
 export default Navbar;
