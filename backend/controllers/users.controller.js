1496f5c7d21606bf1dfb92098192f869f3c33889
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-07-22 15:55:09 +0300

    all of the users routes implemented

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -81,6 +81,231 @@ class UsersController {
       });
     }
   }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({
+          error: true,
+          message: "Cannot authorize user.",
+        });
+      }
+
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        user = await User.findOne({ username: email });
+      }
+
+      if (!user) {
+        return res.status(404).json({
+          error: true,
+          message: "Account not found",
+        });
+      }
+
+      if (!user.active) {
+        return res.status(400).json({
+          error: true,
+          message: "You must verify your email to active your account",
+        });
+      }
+
+      const isValid = await comparePasswords(password, user.password);
+
+      if (!isValid) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid credentials",
+        });
+      }
+
+      const { error, token } = await generateJWT(user.email, user.userId);
+      if (error) {
+        return res.status(500).json({
+          error: true,
+          message: "Couldn't create access token. Please try again later",
+        });
+      }
+
+      user.accessToken = token;
+
+      await user.save();
+
+      return res.send({
+        success: true,
+        message: "User logged in successfully",
+        accessToken: token,
+        username: user.username,
+      });
+    } catch (err) {
+      console.error(`Login error ${err}`);
+      return res.status(500).json({
+        error: true,
+        message: "Couldn't login. Please try again later.",
+      });
+    }
+  }
+
+  async activate(req, res) {
+    try {
+      const { email, code } = req.body;
+      if (!email || !code) {
+        return res.json({
+          error: true,
+          status: 400,
+          message: "Please make a valid request",
+        });
+      }
+      const user = await User.findOne({
+        email: email,
+        emailToken: code,
+        emailTokenExpires: { $gt: Date.now() },
+      });
+
+      if (!user) {
+        return res.status(400).json({
+          error: true,
+          message: "Invalid details",
+        });
+      } else {
+        if (user.active)
+          return res.send({
+            error: true,
+            message: "Account already activated",
+            status: 400,
+          });
+
+        user.emailToken = "";
+        user.emailTokenExpires = null;
+        user.active = true;
+
+        await user.save();
+
+        return res.status(200).json({
+          success: true,
+          message: "Account activated.",
+        });
+      }
+    } catch (error) {
+      console.error("activation-error", error);
+      return res.status(500).json({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+
+  async forgotPassword(req, res) {
+    try {
+      const { email } = req.body;
+      if (!email) {
+        return res.send({
+          status: 400,
+          error: true,
+          message: "Cannot be processed",
+        });
+      }
+      const user = await User.findOne({
+        email: email,
+      });
+
+      let code = Math.floor(100000 + Math.random() * 900000);
+      let response = await sendEmail(user.email, code);
+
+      if (response.error) {
+        return res.status(500).json({
+          error: true,
+          message: "Couldn't send mail. Please try again later.",
+        });
+      }
+
+      let expiry = Date.now() + 60 * 1000 * 15;
+      user.resetPasswordToken = code;
+      user.resetPasswordExpires = expiry; // 15 minutes
+
+      await user.save();
+
+      return res.send({
+        success: true,
+        message: "Please check your email inbox for the reset code",
+      });
+    } catch (error) {
+      console.error("forgot-password-error", error);
+      return res.status(500).json({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+
+  async resetPassword(req, res) {
+    try {
+      const { token, newPassword, confirmPassword } = req.body;
+      if (!token || !newPassword || !confirmPassword) {
+        return res.status(403).json({
+          error: true,
+          message:
+            "Couldn't process request. Please provide all mandatory fields",
+        });
+      }
+      const user = await User.findOne({
+        resetPasswordToken: req.body.token,
+        resetPasswordExpires: { $gt: Date.now() },
+      });
+      if (!user) {
+        return res.send({
+          error: true,
+          message: "Password reset token is invalid or has expired.",
+        });
+      }
+      if (newPassword !== confirmPassword) {
+        return res.status(400).json({
+          error: true,
+          message: "Passwords don't match",
+        });
+      }
+      const hash = await hashPassword(req.body.newPassword);
+      user.password = hash;
+      user.resetPasswordToken = null;
+      user.resetPasswordExpires = "";
+
+      await user.save();
+
+      return res.send({
+        success: true,
+        message: "Password has been changed",
+      });
+    } catch (error) {
+      console.error("reset-password-error", error);
+      return res.status(500).json({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
+
+  async logout(req, res) {
+    try {
+      const { id } = req.decoded;
+
+      let user = await User.findOne({ userId: id });
+
+      user.accessToken = "";
+
+      await user.save();
+
+      return res.send({ success: true, message: "User Logged out" });
+    } catch (error) {
+      console.error("user-logout-error", error);
+      return res.stat(500).json({
+        error: true,
+        message: error.message,
+      });
+    }
+  }
 }
 
 export default UsersController;
--- backend/routes/users.router.js
+++ backend/routes/users.router.js
@@ -1,5 +1,8 @@
 import express from "express";
 
+import cleanBody from "../middlewares/cleanbody.js";
+import validateToken from "../middlewares/validateToken.js";
+
 import UsersController from "../controllers/users.controller.js";
 
 const usersRouter = express.Router();
@@ -7,7 +10,11 @@ const usersRouter = express.Router();
 const usersController = new UsersController();
 
 usersRouter.get("/", usersController.greetUser);
-
-usersRouter.post("/signup", usersController.signup);
+usersRouter.post("/signup", cleanBody, usersController.signup);
+usersRouter.post("/login", cleanBody, usersController.login);
+usersRouter.patch("/activate", cleanBody, usersController.activate);
+usersRouter.patch("/forgot", cleanBody, usersController.forgotPassword);
+usersRouter.patch("/reset", cleanBody, usersController.resetPassword);
+usersRouter.patch("/logout", validateToken, usersController.logout);
 
 export default usersRouter;
